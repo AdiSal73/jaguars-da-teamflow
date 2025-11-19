2e6faaff66cdc88d65c554925a0b53a0237bc48c
Tim Salmoni <tim.salmo@gmail.com>
2025-11-19 08:41:00 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -3,7 +3,6 @@ import Players from './pages/Players';
 import PlayerProfile from './pages/PlayerProfile';
 import BookSession from './pages/BookSession';
 import Teams from './pages/Teams';
-import Evaluations from './pages/Evaluations';
 import Assessments from './pages/Assessments';
 import TeamDashboard from './pages/TeamDashboard';
 import ClubManagement from './pages/ClubManagement';
@@ -11,15 +10,12 @@ import TrainingPlans from './pages/TrainingPlans';
 import TrainingPlanDetail from './pages/TrainingPlanDetail';
 import Messages from './pages/Messages';
 import UnassignedRecords from './pages/UnassignedRecords';
-import PlayersTable from './pages/PlayersTable';
 import CoachManagement from './pages/CoachManagement';
-import TeamDetail from './pages/TeamDetail';
 import Availability from './pages/Availability';
 import UserManagement from './pages/UserManagement';
 import Analytics from './pages/Analytics';
 import PublicBooking from './pages/PublicBooking';
 import PlayerAssessmentAnalytics from './pages/PlayerAssessmentAnalytics';
-import EvaluationsTable from './pages/EvaluationsTable';
 import BookingsTable from './pages/BookingsTable';
 import CoachDashboard from './pages/CoachDashboard';
 import TeamCalendar from './pages/TeamCalendar';
@@ -39,7 +35,6 @@ export const PAGES = {
     "PlayerProfile": PlayerProfile,
     "BookSession": BookSession,
     "Teams": Teams,
-    "Evaluations": Evaluations,
     "Assessments": Assessments,
     "TeamDashboard": TeamDashboard,
     "ClubManagement": ClubManagement,
@@ -47,15 +42,12 @@ export const PAGES = {
     "TrainingPlanDetail": TrainingPlanDetail,
     "Messages": Messages,
     "UnassignedRecords": UnassignedRecords,
-    "PlayersTable": PlayersTable,
     "CoachManagement": CoachManagement,
-    "TeamDetail": TeamDetail,
     "Availability": Availability,
     "UserManagement": UserManagement,
     "Analytics": Analytics,
     "PublicBooking": PublicBooking,
     "PlayerAssessmentAnalytics": PlayerAssessmentAnalytics,
-    "EvaluationsTable": EvaluationsTable,
     "BookingsTable": BookingsTable,
     "CoachDashboard": CoachDashboard,
     "TeamCalendar": TeamCalendar,
